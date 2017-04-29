76b417af1bc60e61c19fd4a6ac01453d33e0d557
Gnyanendra.Nath <gnyanendra.nath.madishetty@oracle.com>
2017-04-29 22:38:25 +0530

    adding password as well

--- engineering-everything-core/src/main/groovy/repositories/UserRepository.groovy
+++ engineering-everything-core/src/main/groovy/repositories/UserRepository.groovy
@@ -11,7 +11,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  */
 
 public interface UserRepository extends MongoRepository<User,String> {
-    public User findByFirstName(String firstName);
     public User findByEmail(String email);
-    public List<User> findByLastName(String lastName);
 }
